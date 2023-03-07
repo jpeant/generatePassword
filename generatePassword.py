2aae443f241c9fa6aa52dbd3bb26a562be2e20d0
jpeant <jussi_antikainen@hotmail.com>
2023-03-07 22:24:15 +0200

    update

---
+++ generatePassword.py
@@ -0,0 +1,10 @@
+####################################################
+#   Name:   generatePassword.py
+#   Usage:  py %filename
+#
+#   Args:   (length 8 - 24, complexity level 1 - 3)
+#
+#   Date:   7.3.2023
+#   Author: jpeant
+#####################################################
+
